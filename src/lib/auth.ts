024e49c5904550c770f9bea1ca02e18513710714
Christian Sanchez <christian.s.molina@gmail.com>
2026-04-10 01:03:49 -0600

    feat: Add baseUrl to better auth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,6 +9,7 @@ import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL,
     database: drizzleAdapter(db, {
         provider: 'pg',
         usePlural: true,
